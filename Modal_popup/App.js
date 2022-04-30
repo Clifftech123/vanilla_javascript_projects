18fcea2a8ed0c144611087a64725f2757cacd9cc
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-04-30 05:31:01 +0000

    i completed the frist step of the project

---
+++ Modal_popup/App.js
@@ -0,0 +1,23 @@
+// @ts-nocheck
+// VARIABLES
+const modal = document.querySelector('.modal'),
+ btn =document.getElementById('btn'),
+  close = document.querySelector('.close');
+
+//BTN EVENT LISTENER
+btn.addEventListener('click',openModal);
+close.addEventListener('click',closeModal);
+modal.addEventListener('click',closeModal);
+
+//FUNCTIONS 
+
+// open modal function
+function openModal(e){
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+// close modal function
+function closeModal(){ 
+    modal.style.display = "none";
+}

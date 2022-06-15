a0ccc98760102c87db291e0575a2f3790204cdba
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-06-15 21:24:22 +0000

    I added some styling with dar and ligth stlye

---
+++ JS_Dark_mode/App.js
@@ -0,0 +1,19 @@
+// accessing the dom to get all the htm tags
+
+const themeToggle = document.getElementById('theme-toggle');
+
+
+// add event listener to the theme toggle
+themeToggle.onclick = () => {
+    themeToggle.classList.toggle('fa-sun');
+    
+    if (themeToggle.classList.contains('fa-sun')) {
+    document.body.classList.add('active');
+}else {
+    document.body.classList.remove('active');
+}
+
+}
+
+
+// the if statement will check if the theme is dark or light
